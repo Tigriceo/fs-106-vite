23ade90d5029d1a5a7c68a4a4674c6131427f059
Tigriceo <koval.ss92@gmail.com>
2024-03-22 22:22:50 +0200

    ls3

---
+++ src/js/scroll-config.js
@@ -0,0 +1,60 @@
+console.log('test');
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-menu-open]"),
+        closeModalBtn: document.querySelector("[data-menu-close]"),
+        mobilemenu: document.querySelector("[data-menu]"),
+    };
+    if (refs.openModalBtn && refs.closeModalBtn) {
+        refs.openModalBtn.addEventListener("click", toggleModal);
+        refs.closeModalBtn.addEventListener("click", toggleModal);
+    }
+
+    function toggleModal() {
+        refs.mobilemenu.classList.toggle("is-open");
+    }
+    /**
+     * Scroll to section
+     */
+    function scrollToSection() {
+        function smoothScroll(targetEl, duration) {
+            let target = document.querySelector(targetEl);
+            if (!target) {
+                console.log('елемент куди скролити не знайдено:', targetEl);
+                return;
+            }
+            let targetPosition = target.getBoundingClientRect().top;
+            let startPosition = window.pageYOffset;
+            let startTime = null;
+            const ease = function (t, b, c, d) {
+                t /= d / 2;
+                if (t < 1) return c / 2 * t * t + b;
+                t--;
+                return -c / 2 * (t * (t - 2) - 1) + b;
+            };
+            const animation = function (currentTime) {
+                if (startTime === null) startTime = currentTime;
+                const timeElapsed = currentTime - startTime;
+                const run = ease(timeElapsed, startPosition, targetPosition, duration);
+                window.scrollTo(0, run);
+                if (timeElapsed < duration) requestAnimationFrame(animation);
+            };
+            requestAnimationFrame(animation);
+        };
+        const links = document.querySelectorAll('a.scroll-to');
+        if (links) {
+            links.forEach(link => {
+                link.addEventListener('click', function (e) {
+                    e.preventDefault();
+                    const currentTarget = this.getAttribute('href');
+                    if (refs.mobilemenu) {
+                        refs.mobilemenu.classList.contains('is-open') ? refs.modal.classList.remove('is-open') : null;
+                    }
+                    smoothScroll(currentTarget, 1000);
+                });
+            });
+        }
+    }
+    scrollToSection();
+})();
